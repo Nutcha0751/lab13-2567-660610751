4cbf46150fc00d7e8f9f509395288dccc5ea75de
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-09-04 16:26:51 +0700

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Nutcha Khampoung"
+            studentId="660610751"
           />
         </div>
       </body>
